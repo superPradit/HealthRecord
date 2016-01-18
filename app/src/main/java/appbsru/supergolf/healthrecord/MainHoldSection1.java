a24392393a641ea682da9b528158feaa214991bb
golf <golfpradit@gmail.com>
2016-01-18 13:22:27 +0700

    Intent to Section1 finish

--- app/src/main/java/appbsru/supergolf/healthrecord/MainHoldSection1.java
+++ app/src/main/java/appbsru/supergolf/healthrecord/MainHoldSection1.java
@@ -1,13 +1,51 @@
 package appbsru.supergolf.healthrecord;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Switch;
 
-public class MainHoldSection1 extends AppCompatActivity {
+public class MainHoldSection1 extends AppCompatActivity implements View.OnClickListener {
+
+    //explicit
+    private ImageView section1ImageView, section2ImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_hold_section1);
+
+        //Bind Widget
+        bindwidget();
+
+        //Image Controller
+        imageController();
+    } //Main method
+
+    private void imageController() {
+        section1ImageView.setOnClickListener(this);
+        section2ImageView.setOnClickListener(this);
     }
-}
+
+    private void bindwidget() {
+
+        section1ImageView = (ImageView) findViewById(R.id.imageView5);
+        section2ImageView = (ImageView) findViewById(R.id.imageView6);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.imageView5:
+                startActivity(new Intent(MainHoldSection1.this, Section1Activity.class));
+                break;
+            case R.id.imageView6:
+                break;
+        }// switch
+
+    } // onClick
+} //Main Class
