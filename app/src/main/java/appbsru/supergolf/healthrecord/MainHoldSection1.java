7f66c6e0c287248641d6b2b53f834321a4cbb90e
golf <golfpradit@gmail.com>
2016-01-21 10:01:17 +0700

    desigh UI section11 finish

--- app/src/main/java/appbsru/supergolf/healthrecord/MainHoldSection1.java
+++ app/src/main/java/appbsru/supergolf/healthrecord/MainHoldSection1.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainHoldSection1 extends AppCompatActivity implements View.OnClickListener {
 
     //explicit
     private ImageView section1ImageView, section2ImageView;
+
+    private TextView section11TextView, section12TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +30,16 @@ public class MainHoldSection1 extends AppCompatActivity implements View.OnClickL
     private void imageController() {
         section1ImageView.setOnClickListener(this);
         section2ImageView.setOnClickListener(this);
+        section11TextView.setOnClickListener(this);
+        section12TextView.setOnClickListener(this);
     }
 
     private void bindwidget() {
 
         section1ImageView = (ImageView) findViewById(R.id.imageView5);
         section2ImageView = (ImageView) findViewById(R.id.imageView6);
+        section11TextView = (TextView) findViewById(R.id.textView7);
+        section12TextView = (TextView) findViewById(R.id.textView8);
     }
 
     @Override
@@ -47,6 +54,15 @@ public class MainHoldSection1 extends AppCompatActivity implements View.OnClickL
                 startActivity(new Intent(MainHoldSection1.this, Section12Activity.class));
                 break;
         }// switch
+
+        switch (v.getId()) {
+            case R.id.textView7:
+                startActivity(new Intent(MainHoldSection1.this, Section1Activity.class));
+                break;
+            case R.id.textView8:
+                startActivity(new Intent(MainHoldSection1.this, Section12Activity.class));
+                break;
+        }//switch
 
     } // onClick
 } //Main Class
