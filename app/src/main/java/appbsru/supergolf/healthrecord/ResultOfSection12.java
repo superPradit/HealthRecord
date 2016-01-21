d4901550b587faae8682ffd2cb01af6395e44a4e
golf <golfpradit@gmail.com>
2016-01-21 12:39:53 +0700

    Section12Activity finish

---
+++ app/src/main/java/appbsru/supergolf/healthrecord/ResultOfSection12.java
@@ -0,0 +1,21 @@
+package appbsru.supergolf.healthrecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class ResultOfSection12 extends AppCompatActivity {
+
+    //Explicit
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_of_section12);
+
+
+
+    } //Main Method
+
+} //Main Class
