1c26747c577421e3198d83b67ba76debce34933a
golf <golfpradit@gmail.com>
2016-01-23 21:00:39 +0700

    Design UI การดูแลผิวหนัง finish

--- app/src/main/java/appbsru/supergolf/healthrecord/Section2Activity.java
+++ app/src/main/java/appbsru/supergolf/healthrecord/Section2Activity.java
@@ -69,6 +69,7 @@ public class Section2Activity extends AppCompatActivity implements View.OnClickL
                 startActivity(new Intent(Section2Activity.this, Section2Sup41Activity.class));
                 break;
             case R.id.perform5Button:
+                startActivity(new Intent(Section2Activity.this, Section2Sup51Activity.class));
                 break;
             case R.id.perform6Button:
                 startActivity(new Intent(Section2Activity.this, Section2Hold4Activity.class));
