118b841a0f14f3e72fac4a7e96dfcefb42cea062
golf <golfpradit@gmail.com>
2016-01-24 17:33:32 +0700

    Design UI โรคความดันโลหิตสูง finish

--- app/src/main/java/appbsru/supergolf/healthrecord/Section2Hold7Activity.java
+++ app/src/main/java/appbsru/supergolf/healthrecord/Section2Hold7Activity.java
@@ -1,5 +1,6 @@
 package appbsru.supergolf.healthrecord;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -50,6 +51,7 @@ public class Section2Hold7Activity extends AppCompatActivity implements View.OnC
 
         switch (v.getId()) {
             case R.id.cp1Button:
+                startActivity(new Intent(Section2Hold7Activity.this, Section2Sup91Activity.class));
                 break;
             case R.id.cp2Button:
                 break;
