0f6632aed556d1d935e89d7f8091bba09c1ce62c
golf <golfpradit@gmail.com>
2016-01-24 14:12:16 +0700

    Design UI อาการภาวะคีโตซีส finish

--- app/src/main/java/appbsru/supergolf/healthrecord/Section2Sup81Activity.java
+++ app/src/main/java/appbsru/supergolf/healthrecord/Section2Sup81Activity.java
@@ -1,5 +1,6 @@
 package appbsru.supergolf.healthrecord;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -42,6 +43,7 @@ public class Section2Sup81Activity extends AppCompatActivity implements View.OnC
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.kts1Button:
+                startActivity(new Intent(Section2Sup81Activity.this, Section2Sup811Activity.class));
                 break;
             case R.id.kts2Button:
                 break;
