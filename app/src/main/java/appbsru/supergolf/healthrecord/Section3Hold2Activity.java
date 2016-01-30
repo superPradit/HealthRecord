9cde00d1c8bf286b915af345fe502ba1c1213ae1
golf <golfpradit@gmail.com>
2016-01-30 14:46:32 +0700

    Design UI สูตรอาหาร20 finish

--- app/src/main/java/appbsru/supergolf/healthrecord/Section3Hold2Activity.java
+++ app/src/main/java/appbsru/supergolf/healthrecord/Section3Hold2Activity.java
@@ -105,6 +105,8 @@ public class Section3Hold2Activity extends Activity {
                         startActivity(intent);
                         break;
                     case 19:
+                        intent = new Intent(getApplicationContext(), Section3Sup220Activity.class);
+                        startActivity(intent);
                         break;
                     case 20:
                         break;
