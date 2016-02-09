afef23417336b8b89e3535d6ea67cbf1ec1b47b9
golf <golfpradit@gmail.com>
2016-02-09 22:58:06 +0700

    design UI อัพเดทข้อมูลแพทย์ finish

--- app/src/main/java/appbsru/supergolf/healthrecord/DatabaseDoctor.java
+++ app/src/main/java/appbsru/supergolf/healthrecord/DatabaseDoctor.java
@@ -28,8 +28,22 @@ public class DatabaseDoctor extends SQLiteOpenHelper {
         db.execSQL("CREATE TABLE " + TABLE_NAME + " (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + COL_NAME + " TEXT, " + COL_HOSPITAL + " TEXT, " + COL_SUGGEST + " TEXT, " + COL_PHONE + " TEXT);");
         db.execSQL("INSERT INTO " + TABLE_NAME + " (" + COL_NAME + ", " + COL_HOSPITAL + ", " + COL_SUGGEST + " , "
-                + COL_PHONE + ") VALUES ('นพ.สิทธิผล ชินพงศ์'" + ", 'โรงพยาบาลกรุงเทพ', 'อายุรศาสตร์โรคต่อมไร้ท่อและเมตะบอลิสม','027551129');");
+                + COL_PHONE + ") VALUES ('นพ.สิทธิผล ชินพงศ์', 'โรงพยาบาลกรุงเทพ', 'อายุรศาสตร์โรคต่อมไร้ท่อและเมตะบอลิสม','027551129');");
+        db.execSQL("INSERT INTO " + TABLE_NAME + " (" + COL_NAME + ", " + COL_HOSPITAL + ", " + COL_SUGGEST + " , "
+                + COL_PHONE + ") VALUES ('ศ.นพ.ชัชลิต รัตรสาร', 'โรงพยาบาลพญาไท 2', 'อายุรศาสตร์','1772');");
+        db.execSQL("INSERT INTO " + TABLE_NAME + " (" + COL_NAME + ", " + COL_HOSPITAL + ", " + COL_SUGGEST + " , "
+                + COL_PHONE + ") VALUES ('นพ.วรเสริฐ อิศราธรรม', 'โรงพยาบาลพญาไท 1', 'อายุรศาสตร์','1772');");
+        db.execSQL("INSERT INTO " + TABLE_NAME + " (" + COL_NAME + ", " + COL_HOSPITAL + ", " + COL_SUGGEST + " , "
+                + COL_PHONE + ") VALUES ('พญ.จุลี จตุวรพัฒน์', 'โรงพยาบาลปิยะเวท', 'อายุรกรรมต่อมไร้ท่อและเมตาบอลิซึม','026256555');");
+        db.execSQL("INSERT INTO " + TABLE_NAME + " (" + COL_NAME + ", " + COL_HOSPITAL + ", " + COL_SUGGEST + " , "
+                + COL_PHONE + ") VALUES ('พญ.อารยา ทองผิว', 'โรงพยาบาลเปาโล', 'แพทย์ผู้เชี่ยวชาญเฉพาะด้านเบาหวานและต่อมไร้ท่อ','022717000');");
+        db.execSQL("INSERT INTO " + TABLE_NAME + " (" + COL_NAME + ", " + COL_HOSPITAL + ", " + COL_SUGGEST + " , "
+                + COL_PHONE + ") VALUES ('ศ.นพ.บุญส่ง องค์พิพัฒนกุล', 'ศูนย์การแพทย์สิริกิติ์', 'อายุรศาสตร์','022012211');");
+        db.execSQL("INSERT INTO " + TABLE_NAME + " (" + COL_NAME + ", " + COL_HOSPITAL + ", " + COL_SUGGEST + " , "
+                + COL_PHONE + ") VALUES ('ศ.นพ.เทพ หิมะทองคำ', 'โรงพยาบาลเทพธารินทร์', 'อายุรกรรม – โรคต่อมไร้ท่อ','023487000');");
     }
+
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
