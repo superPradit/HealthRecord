1d5adb8d6420ad9d981bb832243f1371805872cb
golf <golfpradit@gmail.com>
2016-02-11 18:14:42 +0700

    Design UI ยาแผนปัจจุบัน finish

--- app/src/main/java/appbsru/supergolf/healthrecord/Section5Sup1Activity.java
+++ app/src/main/java/appbsru/supergolf/healthrecord/Section5Sup1Activity.java
@@ -1,7 +1,12 @@
 package appbsru.supergolf.healthrecord;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class Section5Sup1Activity extends AppCompatActivity {
 
@@ -9,5 +14,54 @@ public class Section5Sup1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section5_sup1);
+
+        String[] list = {"GLIPIZIDE", "GLIBENCLAMIDE", "REPAGLINIDE", "METFORMIN", "PIOGLITAZONE"
+                , "ACARBOSE", "VOGLIBOSE", "INSULIN"};
+
+        CustomAdapter adapter = new CustomAdapter(getApplicationContext(), list);
+
+        ListView listView = (ListView) findViewById(R.id.listDrug1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                Intent intent;
+                switch (arg2) {
+                    case 0:
+                        intent = new Intent(getApplicationContext(), Section5Sup11Activity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent = new Intent(getApplicationContext(), Section5Sup12Activity.class);
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        intent = new Intent(getApplicationContext(), Section5Sup13Activity.class);
+                        startActivity(intent);
+                        break;
+                    case 3:
+                        intent = new Intent(getApplicationContext(), Section5Sup14Activity.class);
+                        startActivity(intent);
+                        break;
+                    case 4:
+                        intent = new Intent(getApplicationContext(), Section5Sup15Activity.class);
+                        startActivity(intent);
+                        break;
+                    case 5:
+                        intent = new Intent(getApplicationContext(), Section5Sup16Activity.class);
+                        startActivity(intent);
+                        break;
+                    case 6:
+                        intent = new Intent(getApplicationContext(), Section5Sup17Activity.class);
+                        startActivity(intent);
+                        break;
+                    case 7:
+                        intent = new Intent(getApplicationContext(), Section5Sup18Activity.class);
+                        startActivity(intent);
+                        break;
+                }
+
+            }
+        });
+
     }
 }
